33dfeb10e32a5dfee299e769e97aaa8cefd785a2
maryialis <myaw@tut.by>
2017-05-24 16:14:50 +0300

    CourseResult controller and routes added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/source_providers/:id', to: 'source_provider#show'
   get '/users', to: 'user#index'
   get '/users/:id', to: 'user#show'
+  get '/courses', to: 'course_result#current'
+  get '/courses/history', to: 'course_result#history'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
