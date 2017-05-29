a339ac42f7571a7044d6315391e341a175ac7bff
maryialis <myaw@tut.by>
2017-05-29 13:10:19 +0300

    User: password length validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  validates :email, presence:true, uniqueness:true, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
-  validates :password, presence:true
+  REASONABLE_PASSWORD_LIMIT = (6..100)
+  validates :email, presence:true, uniqueness:{case_sensitive: false}, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
+  validates :password, presence:true, length: {in: REASONABLE_PASSWORD_LIMIT}
+  validate :prepare_names
   
   def full_name
     first_name + ' ' + last_name
@@ -9,18 +11,18 @@ class User < ApplicationRecord
   before_save :prepare
 
   private
-  def prepare
-    email.downcase!
-    if (first_name.nil? && last_name.nil?)
+  
+  def prepare_names
+    if (!first_name.present? && !last_name.present?)
       self.first_name = (0...8).map { (65 + rand(26)).chr }.join
       self.last_name = (0...8).map { (65 + rand(26)).chr }.join
     end
-    #puts "First name: #{first_name} Last name: #{last_name}"
-    if (first_name.nil? || last_name.nil?)
-      throw :abort
-    else
-      first_name.capitalize!
-      last_name.capitalize!
-    end
+    errors.add(:base, "first_name or last_name is not set") unless (first_name.present? && last_name.present?)
+  end
+  
+  def prepare
+    email.downcase!
+    first_name.capitalize!
+    last_name.capitalize!
   end
 end
