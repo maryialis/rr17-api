8703e43cc23c70a24b9ce080f7d872de8aa9e488
maryialis <myaw@tut.by>
2017-05-31 09:09:40 +0300

    SourceProvider/User association: belongs_to -> has_many; display author in json output

--- app/models/source_provider.rb
+++ app/models/source_provider.rb
@@ -1,7 +1,13 @@
 class SourceProvider < ApplicationRecord
   alias_attribute :author, :user_id
-  belongs_to :author, class_name: 'User', foreign_key: :user_id
+  has_one :author, class_name: 'User', foreign_key: "user_id"
   has_many :course_results, dependent: :destroy
   validates :name, :url, :address, presence: true
   validates :name, :url, uniqueness: true
+  
+  def as_json(options = {})
+    options[:methods] ||= []
+    options[:methods] += [:author]
+    super#(options.merge({ except: [:user_id] }))
+  end
 end
