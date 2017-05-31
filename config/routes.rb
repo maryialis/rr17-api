42fb9a6e1f3a538fed0dc36078f78a0b82881572
maryialis <myaw@tut.by>
2017-05-31 10:25:54 +0300

    course results routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   resources 'source_providers'
   resources 'users'
   resources 'source_parsers'
-  get '/courses', to: 'course_result#current'
-  get '/courses/history', to: 'course_result#history'
+  get '/courses', to: 'course_results#current'
+  get '/courses/history', to: 'course_results#history'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
