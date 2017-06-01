4bfa267ab73f853fabec984078cb3c47fb7074e7
maryialis <myaw@tut.by>
2017-06-01 17:51:06 +0300

    SourceParser model: helper source_parser_from_source_provider method added

--- app/models/source_parser.rb
+++ app/models/source_parser.rb
@@ -1,3 +1,8 @@
 class SourceParser < ApplicationRecord
   validates :parser_name, :url_pattern, presence: true
+  
+  def self.source_parser_from_source_provider (source_provider)
+    p = SourceParser.all.detect {|parser| source_provider.url =~ /#{parser.url_pattern}/i}
+    p.try(:parser_name)
+  end
 end
