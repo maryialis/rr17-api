2d20bb0e00922b2893e88bc568fe8e9b53d5d310
maryialis <myaw@tut.by>
2017-06-02 22:11:54 +0300

    controllers moved to V1 namespace

---
+++ app/controllers/v1/source_parsers_controller.rb
@@ -0,0 +1,51 @@
+module V1
+
+  class SourceParsersController < ApiBaseController
+    def index
+      render json: SourceParser.all
+    end
+
+    def show
+      rec = SourceParser.find_by_id(params[:id])
+      if rec
+        render json: rec
+      else
+        render json: {errors: "SourceParser not found", status: 400}, status: 400
+      end
+    end
+
+    def create
+      rec = SourceParser.create(source_parser_params)
+      if rec.valid?
+        render json: rec
+      else
+        render json: {errors: rec.errors, status: 400}, status: 400
+      end
+    end
+
+    def destroy
+      rec = SourceParser.find_by_id(params[:id])
+      rec.destroy if rec
+    end
+
+    def update
+      rec = SourceParser.find_by_id(params[:id])
+      if rec
+        rec.update(source_parser_params)
+        if rec.valid?
+          render json: rec
+        else
+          render json: {errors: rec.errors, status: 400}, status: 400
+        end
+      else
+        render json: {errors: "SourceParser not found", status: 400}, status: 400
+      end
+    end
+    
+    private
+    def source_parser_params
+      params.require(:source_parser).permit(:parser_name, :url_pattern)
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
-  root to: 'source_providers#home'
-  get 'source_providers/parse_all', to: 'source_providers#parse_all'
-  resources 'source_providers'
-  get 'source_providers/:id/start_now', to: 'source_providers#parse_now'
-  resources 'users'
-  resources 'source_parsers'
-  get '/courses', to: 'course_results#current'
-  get '/courses/history', to: 'course_results#history'
-
+  namespace :v1 do
+    root to: 'source_providers#home'
+    get 'source_providers/parse_all', to: 'source_providers#parse_all'
+    resources 'source_providers'
+    get 'source_providers/:id/start_now', to: 'source_providers#parse_now'
+    resources 'users'
+    resources 'source_parsers'
+    get '/courses', to: 'course_results#current'
+    get '/courses/history', to: 'course_results#history'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
