b72dc5efbd1d5cd3e182a636c25062386269d296
maryialis <myaw@tut.by>
2017-06-07 20:55:33 +0300

    use POST method for parse requests

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   namespace :v1 do
     root to: 'source_providers#home'
-    get 'source_providers/parse_all', to: 'source_providers#parse_all'
     resources 'source_providers'
-    get 'source_providers/:id/start_now', to: 'source_providers#parse_now'
+    post 'source_providers/:id/start_now', to: 'source_providers#parse_now'
+    post 'source_providers/parse_all', to: 'source_providers#parse_all'
     resources 'users'
     resources 'source_parsers'
     get '/courses', to: 'course_results#current'
